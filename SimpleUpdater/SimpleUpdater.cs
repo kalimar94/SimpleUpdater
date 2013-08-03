7d1b2842b7307add62eeb208397f36498fe0a0a1
kalimar94 <kalimar@abv.bg>
2013-08-03 17:40:31 +0300

    Providing an actual update file

--- SimpleUpdater/SimpleUpdater.cs
+++ SimpleUpdater/SimpleUpdater.cs
@@ -8,11 +8,8 @@ class Example
     {
         Updater.CheckForUpdatesAsync();
 
-        // Read input
-        while (true)
-        {
-            Console.ReadLine();
-        }
+        // keep the program alive while checking for updates
+        Console.ReadKey();
     }
 
 
@@ -21,7 +18,7 @@ class Example
 class Updater
 {
     static decimal currentVersion = 1.01m;
-    static string versionFile = "https://github.com/kalimar94/SimpleUpdater/SimpleUpdater/version.ver";
+    static string versionFile = "https://raw.github.com/kalimar94/SimpleUpdater/master/SimpleUpdater/version.ver";
     public static void CheckForUpdates()
     {
         try
@@ -47,7 +44,7 @@ class Updater
 
     }
 
-    // This is an async method it works in the background, it allows your program to continiue while updating is being processed in the background
+    // This is an async method - it works in the background, it allows your program to continiue while updating is being processed in the background
     public async static void CheckForUpdatesAsync()
     {
         try
@@ -79,6 +76,7 @@ class Updater
     {
         using (WebClient downloader = new WebClient())
         {
+            Console.WriteLine("Downloading updates or whatsoever");
            // downloader.DownloadFile("adress", System.IO.Directory.GetCurrentDirectory());  // download a file to the current directory
         }
     }
